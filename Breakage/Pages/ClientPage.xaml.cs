8fbdd334e46fb28a9e30ba8067b746a4e86e96a5
Misha <gutufik@mail.ru>
2023-02-17 19:27:32 +0300

    Add project files.

---
+++ Breakage/Pages/ClientPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Breakage.Data;
+using Microsoft.Win32;
+
+namespace Breakage.Pages
+{
+    /// <summary>
+    /// Interaction logic for ClientPage.xaml
+    /// </summary>
+    public partial class ClientPage : Page
+    {
+        public Client Client { get; set; }
+        public List<Gender> Genders { get; set; }
+        public ClientPage(Client client)
+        {
+            InitializeComponent();
+            Client = client;
+            Genders = DataAccess.GetGenders();
+            DataContext = this;
+        }
+
+        private void btnChangePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog().Value == true)
+            {
+                Client.Photo = File.ReadAllBytes(openFileDialog.FileName);
+                ClientImage.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.DeleteClient(Client);
+            NavigationService.GoBack();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.SaveClient(Client);
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ Breakage/Pages/ClientsListPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Breakage.Data;
+
+namespace Breakage.Pages
+{
+    /// <summary>
+    /// Interaction logic for ClientsListPage.xaml
+    /// </summary>
+    public partial class ClientsListPage : Page
+    {
+        public List<Client> Clients { get; set; }
+        public ClientsListPage()
+        {
+            InitializeComponent();
+            Clients = DataAccess.GetClients();
+
+            DataAccess.RefreshList += DataAccess_RefreshList;
+            DataContext = this;
+        }
+
+        private void DataAccess_RefreshList()
+        {
+            Clients = DataAccess.GetClients();
+            lvClients.ItemsSource = Clients;
+            lvClients.Items.Refresh();
+        }
+
+        private void lvClients_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var client = (Client)lvClients.SelectedItem;
+            if (client != null)
+                NavigationService.Navigate(new ClientPage(client));
+        }
+    }
+}
